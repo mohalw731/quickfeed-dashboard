4d58bf4996e26f1d7e2aec72ef68f81f643b3a3f
mohalw731 <mohalw731@gmail.com>
2024-08-27 04:38:31 +0200

    added a table of all the feedbacks

--- app/(user)/projects/[formId]/page.tsx
+++ app/(user)/projects/[formId]/page.tsx
@@ -1,14 +1,36 @@
-import React from 'react'
+import Table from "@/components/table";
+import { db } from "@/db";
+import { projects as dbProjects } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import React from "react";
 
-export default function page(params: {
-    params: {
-        formId: string
-    }
-} ) {
-    const projectId = params.params.formId
+export default async function page(params: {
+  params: {
+    formId: string;
+  };
+}) {
+  const projectId = params.params.formId;
+  if (!projectId) return <div>Invalid Project ID</div>;
+
+  const projects = await db.query.projects.findMany({
+    where: eq(dbProjects.id, parseInt(projectId)),
+    with: {
+      feedbacks: true,
+    },
+  });
+
+  console.log(projects);
+
   return (
     <div>
-      {projectId}
+      {projects.map((project) => (
+        <div className="">
+          <h1>{project.name}</h1>
+          <p>{project.description}</p>
+          <p>{project.url}</p>
+          <Table data={project.feedbacks} />
+        </div>
+      ))}
     </div>
-  )
+  );
 }
